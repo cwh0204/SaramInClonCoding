cf985065152a49b0dc1e5993cf81f811ba4f2d1b
cwh0204 <cwh0204@nate.com>
2026-01-02 21:38:14 +0900

    12
    12

--- SaraminClonCoding/src/main/java/run/sumin/board/controller/BoardDetailController.java
+++ SaraminClonCoding/src/main/java/run/sumin/board/controller/BoardDetailController.java
@@ -1,8 +1,24 @@
 package run.sumin.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.RequiredArgsConstructor;
+import run.sumin.board.service.BoardService;
+import run.sumin.dto.BoardDTO;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardDetailController {
-
+	
+	private final BoardService service;
+	
+	@GetMapping("/board/select")
+	@ResponseBody
+	public BoardDTO boardSelect(Integer jobId) {
+		
+		return service.selectBoardDetail(jobId);
+	}
 }
--- SaraminClonCoding/src/main/java/run/sumin/board/controller/BoardInsertController.java
+++ SaraminClonCoding/src/main/java/run/sumin/board/controller/BoardInsertController.java
@@ -1,8 +1,22 @@
 package run.sumin.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.RequiredArgsConstructor;
+import run.sumin.board.service.BoardService;
+import run.sumin.dto.BoardDTO;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardInsertController {
-
+	
+	private final BoardService service;
+	
+	@PostMapping("/board/insert")
+	@ResponseBody
+	public int boardInsert(BoardDTO board) {
+		return service.insertBoard(board);
+	}
 }
