2f8e0a81fdf54731459a5d6faeb195e9070f3e6e
cwh0204 <cwh0204@nate.com>
2026-01-02 16:39:26 +0900

    보드 인서트 뷰
    12

--- SaraminClonCoding/src/main/java/run/sumin/board/controller/BoardController.java
+++ SaraminClonCoding/src/main/java/run/sumin/board/controller/BoardController.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 	
 	@GetMapping("/index")
-	public String formUI(){
+	public String formIndex(){
 		return "index";
+	}
+	@GetMapping("/board/view")
+	public String formView(){
+		return "board/boardview";
+	}
+	@GetMapping("/board/insert")
+	public String formInsert(){
+		return "board/boardinsert";
 	}
 }
