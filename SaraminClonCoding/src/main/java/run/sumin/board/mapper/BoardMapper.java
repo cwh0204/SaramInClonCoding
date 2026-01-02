91db602fb23bb639a774caa7c6eb54d4a0ba5a8a
csc5090 <tkdcjf5090@gmail.com>
2026-01-02 17:43:27 +0900

    MAPPER CONFIGURAION

--- SaraminClonCoding/src/main/java/run/sumin/board/mapper/BoardMapper.java
+++ SaraminClonCoding/src/main/java/run/sumin/board/mapper/BoardMapper.java
@@ -1,5 +1,13 @@
 package run.sumin.board.mapper;
 
+import java.util.List;
+
+import run.sumin.dto.BoardDTO;
+
 public interface BoardMapper {
+	int insertBoard(BoardDTO boardDTO);
 	
+	BoardDTO selectBoardDetail(int jobId);
+	
+	List<BoardDTO> selectBoardList();
 }
