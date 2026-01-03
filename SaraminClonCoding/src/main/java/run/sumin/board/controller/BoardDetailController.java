68b38c47390158f74b37ed78ba9b28c67f112a0f
kimgc400-coder <kimgc400@gmail.com>
2026-01-03 17:50:02 +0900

    Update BoardDetailController.java

--- SaraminClonCoding/src/main/java/run/sumin/board/controller/BoardDetailController.java
+++ SaraminClonCoding/src/main/java/run/sumin/board/controller/BoardDetailController.java
@@ -2,7 +2,7 @@ package run.sumin.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
